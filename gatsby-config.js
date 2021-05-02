fb71dbc9312d2598b24cadee1b9896d71a42da66
SeanMee86 <seanmee8186@gmail.com>
2021-05-01 21:33:54 -0700

    install wp/graphql plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,16 +7,9 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
+      `gatsby-plugin-sharp`,
+      {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -28,8 +21,32 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
+    {
+        resolve: `gatsby-source-wordpress`,
+        options: {
+            url: `http://34.222.54.131/graphql`,
+        },
+    },
+    {
+        resolve: `gatsby-source-graphql`,
+        options: {
+          // This type will contain remote schema Query type
+          typeName: `WPGraphQL`,
+          // This is field under which it's accessible
+          fieldName: `wpcontent`,
+          // Url to query from
+          url: `http://34.222.54.131/graphql`,
+        },
+    },
+    {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            name: `images`,
+            path: `${__dirname}/src/images`,
+        },
+    },
+      `gatsby-plugin-gatsby-cloud`,
+      // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
