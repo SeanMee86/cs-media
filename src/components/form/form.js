8271592c94469e1ab9f5adf85faec43204202d77
SeanMee86 <seanmee8186@gmail.com>
2021-09-11 12:39:28 -0700

    updating contact form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -6,21 +6,38 @@ export const Form = () => (
         <form className={formStyles.formContainer} name="contact" method="POST" data-netlify-honeypot="bot-field" data-netlify="true">
             <div>
                 <div className={formStyles.formColumn}>
-                    <input type="hidden" name="form-name" value="contact"/>
-                    <p>
-                        <label className='labels' htmlFor='name'>Full Name:</label>
-                        <input id='name' type="text" name="name"/>
-                    </p>
+                    <div>
+                        <input type="hidden" name="form-name" value="contact"/>
+                        <p style={{marginTop: 0}}>
+                            <label className='labels' htmlFor='name'>Full Name:</label>
+                            <input id='name' type="text" name="name"/>
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            <label className='labels' htmlFor='email'>Email:</label>
+                            <input id='email' type="email" name="email"/>
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            <label className='labels' htmlFor='subject'>Subject:</label>
+                            <input id='subject' type="text" name="subject"/>
+                        </p>
+                    </div>
                 </div>
                 <div className={formStyles.formColumn}>
-                    <p>
-                        <label className='labels' htmlFor='email'>Email:</label>
-                        <input id='email' type="email" name="email"/>
-                    </p>
+                    <div style={{height: '100%'}}>
+                        <p style={{marginTop: 0, height: '100%'}}>
+                            <label className='labels' htmlFor='message'>Message:</label>
+                            <textarea style={{height: '100%', marginBottom: 0, minHeight: '150px'}} id='message' name="message"/>
+                        </p>
+                    </div>
                 </div>
             </div>
             <button className={formStyles.contactBtn} type="submit">REACH OUT!</button>
-        </form></>
+        </form>
+    </>
 )
 
 export default Form;
