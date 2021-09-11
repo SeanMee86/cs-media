0933b9998186f200a9be9541b77d0f155b720238
SeanMee86 <seanmee8186@gmail.com>
2021-09-11 14:09:36 -0700

    add back to top button

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,17 +5,42 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import "./layout.css"
+import * as React from "react";
+import PropTypes from "prop-types";
+import "./layout.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import * as styles from "./form/form.module.scss";
+import {useState} from "react";
 
 const Layout = ({ children }) => {
+
+  const [formStyles, setFormStyles] = useState({
+      maxWidth: 'unset',
+      position: 'fixed',
+      bottom: '-200px',
+      right: '20px',
+      transition: '0.24s bottom ease-in-out'
+  });
+
+  const buttonStyles = {
+      display: 'flex',
+      justifyContent: 'center',
+      fontSize: '42px',
+      height: '60px'
+  }
+
+  const scrollToTop = () => {
+      window.scroll(0, 0);
+  }
+
   return (
     <>
       <div>
         <main>{children}</main>
+        <form style={formStyles}>
+            <button onClick={scrollToTop} style={buttonStyles} className={styles.contactBtn} type={"button"}>&#8963;</button>
+        </form>
       </div>
     </>
   )
