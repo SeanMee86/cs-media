47fb253511f9b5816e5b079ae85dc3882df4d4cc
SeanMee86 <seanmee8186@gmail.com>
2021-10-09 11:10:37 -0700

    update nav with header logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
 import * as React from "react"
 import * as headerStyles from './header.module.scss';
+import * as csLogo from '../images/cs-media-logo.png';
+import {Link} from 'gatsby';
 import Navigation from "./navigation/navigation";
+
+console.log(csLogo)
 
 const Header = () => (
   <header className={headerStyles.header}>
+    <Link to={'/'}><img style={{position: 'absolute', top: '50%', transform: 'translateY(-50%)', left: '50px'}} src={csLogo.default} alt="CS Media Logo"/></Link>
     <div
       style={{
         margin: `0 auto`,
