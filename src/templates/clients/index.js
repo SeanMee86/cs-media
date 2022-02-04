eac345f2df0adb419fa368e3d74bdb00d5cd4d15
SeanMee86 <seanmee8186@gmail.com>
2022-02-03 21:33:11 -0800

    continue work on clients template

--- src/templates/clients/index.js
+++ src/templates/clients/index.js
@@ -3,12 +3,31 @@ import {graphql} from "gatsby";
 import Layout from "../../components/layout";
 import Hero from "../../components/hero/hero";
 import Contact from "../../components/sections/contact/contact";
+import * as styles from "./clients.module.scss"
 
 const ClientsPageTemplate = ({data: {wpClient: {clientsPost}}}) => {
     console.log(clientsPost)
     return (
         <Layout>
             <Hero hasText={true} pageTitle={'Our Work'} />
+            <div>
+                <div className={`${styles.section} ${styles.flexSection}`}>
+                    <img style={{maxWidth: '350px'}} src={clientsPost.mainImage.sourceUrl} alt={clientsPost.mainImage.altText}/>
+                    <div>
+                        <h2>{clientsPost.clientName}</h2>
+                        <p>{clientsPost.clientDescription}</p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.imageBreak}>
+                {clientsPost.imageBreaker.map(image => <div style={{backgroundImage: `url(${image.featuredImage.node.sourceUrl})`}}/>)}
+            </div>
+            <div>
+                <div className={`${styles.section} ${styles.strategySection}`}>
+                    <h2>Strategy</h2>
+                    <p>{clientsPost.strategyCopy}</p>
+                </div>
+            </div>
             <Contact/>
         </Layout>
     )
