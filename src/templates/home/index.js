aef728a384cf744b8bcb9397342cb94689666c37
SeanMee86 <seanmee8186@gmail.com>
2022-03-18 12:32:58 -0700

    remove no index from seo (#6)

--- src/templates/home/index.js
+++ src/templates/home/index.js
@@ -37,7 +37,6 @@ export const query = graphql`
                 metaDesc
                 focuskw
                 metaKeywords
-                metaRobotsNoindex
                 metaRobotsNofollow
                 opengraphTitle
                 opengraphDescription
